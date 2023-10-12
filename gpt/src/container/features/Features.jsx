92ab865548d723e84cf5ab7416ccfcddf2c6f2eb
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-12 19:19:42 +0100

    CREATE: STLYING

--- gpt/src/container/features/Features.jsx
+++ gpt/src/container/features/Features.jsx
@@ -3,6 +3,26 @@ import './features.scss'
 import { Feature } from '../../Components'
 
 
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.'
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly addres.'
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
+  },
+]
+
+
 export default function features() {
   return (
     <div className='gpt3__features section__padding' id='features'>
@@ -11,10 +31,9 @@ export default function features() {
         <p>Request Early Access to Get Started</p>
       </div>
         <div className="gpt3__features-container">
-          <Feature />
-          <Feature />
-          <Feature />
-          <Feature />
+          {featuresData .map((item, index) => (
+            <Feature title={item.title} text={item.text} />
+          ))}
         </div>
     </div>
   )
