cd0b71f1837a2728881faf9194bae20a5dc9ca0e
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-12 13:35:19 +0100

    CREATE: BRAND COMPONENTS & SASS STYLE

--- gpt/src/container/header/header.jsx
+++ gpt/src/container/header/header.jsx
@@ -21,11 +21,10 @@ export default function Header() {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
+      </div>
         <div className="gpt3__header-image">
           <img src={ai} alt={ai} />
         </div>
-      </div>
     </div>
   )
 }
