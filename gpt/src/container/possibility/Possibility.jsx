eefa471fd93a84d91589cadd9adb04d4a1558947
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-12 20:04:36 +0100

    CREATE: STYLING FOR POSSIBILITY

--- gpt/src/container/possibility/Possibility.jsx
+++ gpt/src/container/possibility/Possibility.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Possibility.scss';
-
+import  possibility  from '../../assets/possibility.png';
 const Possibility = () => {
   return (
-    <div>
-      
+    <div className='gpt3__possibility section__padding' id='Possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibility} alt="possibility" />
+      </div>
+      <div>
+        <p></p>
+        <h2></h2>
+        <p></p>
+        <p></p>
+      </div>
     </div>
   )
 }
