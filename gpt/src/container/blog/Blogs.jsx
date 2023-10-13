9c429a56dfdef02a9cc4ea2cd3664cba6b0fa2bd
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 13:45:33 +0100

    DELETE: IMPORTS IN ARTICLE COMPONENT

--- gpt/src/container/blog/Blogs.jsx
+++ gpt/src/container/blog/Blogs.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './blogs.scss'
+import {  blog01, blog02, blog03, blog04, blog05, } from '.imports.js'
+
 
 const Blogs = () => {
   return (
@@ -8,6 +10,15 @@ const Blogs = () => {
         <h1 className='gradient__text'>A lot is happening, 
         We are blogging about it.</h1>
        </div>
+       <div className="gpt3__blog-container_groupA">
+        <Article />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+    </div>
     </div>
   )
 }
