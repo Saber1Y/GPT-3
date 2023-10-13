21ac454865cc08100022be6a6cbe38560fc1ff91
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 13:34:45 +0100

    CREATE: Article component create and import system

--- gpt/src/Components/article/Article.jsx
+++ gpt/src/Components/article/Article.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './article.scss';
+// import {  blog01, blog02, blog03, blog04, blog05, } from './imports.js'
 
 const Article = () => {
   return (
     <div>
-      Article
+     
     </div>
   )
 }
--- gpt/src/container/blog/Blogs.jsx
+++ gpt/src/container/blog/Blogs.jsx
@@ -3,8 +3,11 @@ import './blogs.scss'
 
 const Blogs = () => {
   return (
-    <div>
-      
+    <div className='gpt3__blog section__padding' id='blog'>
+       <div className="gpt3__blog-heading">
+        <h1 className='gradient__text'>A lot is happening, 
+        We are blogging about it.</h1>
+       </div>
     </div>
   )
 }
