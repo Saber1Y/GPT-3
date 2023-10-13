bd7f05da642c5fe88b26082f3c812b6afef4aecd
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 15:20:40 +0100

    UPDATE: STYLYING ADDED

--- gpt/src/Components/article/Article.jsx
+++ gpt/src/Components/article/Article.jsx
@@ -8,6 +8,13 @@ const Article = ({ imgUrl, date, title }) => {
      <div className="gtp3__blog-container_artcile-image">
       <img src={imgUrl} />
      </div>
+     <div className="gpt3__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{title}</h3>
+      </div>
+      <p>Read Full Article</p>
+     </div>
     </div>
   )
 }
