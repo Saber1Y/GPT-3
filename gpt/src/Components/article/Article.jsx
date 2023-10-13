aff2f7a66d8dc537366430b14e693614041f523b
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 15:13:33 +0100

    UPDATE: PASSED PROPS

--- gpt/src/Components/article/Article.jsx
+++ gpt/src/Components/article/Article.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './article.scss';
 
 
-const Article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className='gpt3__blof-container_article'>
      <div className="gtp3__blog-container_artcile-image">
-      <img src={imgUrl}/>
+      <img src={imgUrl} />
      </div>
     </div>
   )
