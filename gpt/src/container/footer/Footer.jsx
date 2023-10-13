f68fdec3eb3e1867637a3ca591dd356e4e87a6e0
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 16:39:56 +0100

    UPDATE: Basic Footer contents

--- gpt/src/container/footer/Footer.jsx
+++ gpt/src/container/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './footer.scss'
+import gptLogo from '../../assets/logo.svg';
 
 
 const Footer = () => {
@@ -10,6 +11,31 @@ const Footer = () => {
       </div>
       <div  className='gpt3__footer-btn'>
        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+       <div className="gpt3__footer-links_logo">
+        <img src={gptLogo} alt="" />
+        <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+       </div>
+       <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>contact</p>
+        <p>Overons</p>
+       </div>
+       <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+       </div>
+       <div className="gpt3__footer-links_div">
+        <h4>Get in Touch</h4>
+        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+       </div>
       </div>
     </div>
   )
